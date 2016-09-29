3252f70cdd442d30266c4abacf9e871fd89012ce
Lou <lfranklin@deloitte.co.uk>
2016-09-29 16:03:33 +0100

    working FB(own app) and GH(demo app) logins

---
+++ src/test/java/com/example/SocialApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class SocialApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	    int num = 1;
+	    assert(num == 1);
+	}
+
+}
