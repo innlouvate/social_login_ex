b6f90cb284ae7c3d02f431abe3ea74bd6a1c6973
Lou <lfranklin@deloitte.co.uk>
2016-09-29 17:46:25 +0100

    refactoring

---
+++ src/main/java/com/example/ClientResources.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
+import org.springframework.boot.context.properties.NestedConfigurationProperty;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+
+/**
+ * Created by lfranklin on 29/09/2016.
+ */
+
+public class ClientResources {
+
+    @NestedConfigurationProperty
+    private AuthorizationCodeResourceDetails client = new AuthorizationCodeResourceDetails();
+
+    @NestedConfigurationProperty
+    private ResourceServerProperties resource = new ResourceServerProperties();
+
+    public AuthorizationCodeResourceDetails getClient() {
+        return client;
+    }
+
+    public ResourceServerProperties getResource() {
+        return resource;
+    }
+}
